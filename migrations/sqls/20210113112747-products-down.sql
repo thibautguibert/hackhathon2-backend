155311419bdf0278ea642f48eea30a0eb4c4cb66
hugoMonchatre <hugo.monchatre@gmail.com>
2021-01-13 12:34:25 +0100

    premiere table

---
+++ migrations/sqls/20210113112747-products-down.sql
@@ -0,0 +1,26 @@
+/* Replace with your SQL commands */
+DELETE TABLE products ;
+
+DELETE FROM products WHERE id= 1;
+DELETE FROM products WHERE id= 2;
+DELETE FROM products WHERE id= 3;
+DELETE FROM products WHERE id= 4;
+DELETE FROM products WHERE id= 5;
+DELETE FROM products WHERE id= 6;
+DELETE FROM products WHERE id= 7;
+DELETE FROM products WHERE id= 8;
+DELETE FROM products WHERE id= 9;
+DELETE FROM products WHERE id= 10;
+DELETE FROM products WHERE id= 11;
+DELETE FROM products WHERE id= 12;
+DELETE FROM products WHERE id= 13;
+DELETE FROM products WHERE id= 14;
+DELETE FROM products WHERE id= 15;
+DELETE FROM products WHERE id= 16;
+DELETE FROM products WHERE id= 17;
+DELETE FROM products WHERE id= 18;
+DELETE FROM products WHERE id= 19;
+DELETE FROM products WHERE id= 20;
+DELETE FROM products WHERE id= 21;
+DELETE FROM products WHERE id= 22;
+DELETE FROM products WHERE id= 23;
---
+++ migrations/sqls/20210113112747-products-up.sql
@@ -0,0 +1,29 @@
+/* Replace with your SQL commands */
+CREATE TABLE products(
+   id       INTEGER  NOT NULL PRIMARY KEY AUTO_INCREMENT
+  ,name     VARCHAR(100) NOT NULL
+  ,category VARCHAR(100) NOT NULL
+);
+INSERT INTO products(id,name,category) VALUES (1,'Blé Standard 76-220-11','ble');
+INSERT INTO products(id,name,category) VALUES (2,'Epeautre Non Décortiqué','ble');
+INSERT INTO products(id,name,category) VALUES (3,'Blé C2','ble');
+INSERT INTO products(id,name,category) VALUES (4,'Blé Fourrager Ps: 74','ble');
+INSERT INTO products(id,name,category) VALUES (5,'Blé Dur Aux Normes','ble');
+INSERT INTO products(id,name,category) VALUES (6,'Blé Améliorant','ble');
+INSERT INTO products(id,name,category) VALUES (7,'Avoine Noire Ps48/50','avoine');
+INSERT INTO products(id,name,category) VALUES (8,'Avoine Noire Ps52','avoine');
+INSERT INTO products(id,name,category) VALUES (9,'Triticale','triticale');
+INSERT INTO products(id,name,category) VALUES (10,'Orge Fourragère Ps 62-63','orge');
+INSERT INTO products(id,name,category) VALUES (11,'Orge Fourragère Ps 60','orge');
+INSERT INTO products(id,name,category) VALUES (12,'Orge Fourragère Variété Spécifique','orge');
+INSERT INTO products(id,name,category) VALUES (13,'Orge Avec 9% Maximum De Blé','orge');
+INSERT INTO products(id,name,category) VALUES (14,'Maïs Grain','mais');
+INSERT INTO products(id,name,category) VALUES (15,'Maïs Rond','mais');
+INSERT INTO products(id,name,category) VALUES (16,'Pois Jaunes','pois');
+INSERT INTO products(id,name,category) VALUES (17,'Pois Fourragers','pois');
+INSERT INTO products(id,name,category) VALUES (18,'Pois Verts','pois');
+INSERT INTO products(id,name,category) VALUES (19,'colza','colza');
+INSERT INTO products(id,name,category) VALUES (20,'Tournesol Standard','tournesol');
+INSERT INTO products(id,name,category) VALUES (21,'Tournesol Oléique','tournesol');
+INSERT INTO products(id,name,category) VALUES (22,'Féveroles Fourragères','feverol');
+INSERT INTO products(id,name,category) VALUES (23,'Féveroles C2','feverol');
